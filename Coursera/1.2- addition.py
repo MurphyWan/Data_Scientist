1e9fd13467243156735b47754b9a2dfb0ffd8bf3
Murphy Fei Wan <MurphyWan@users.noreply.github.com>
2017-10-24 22:02:40 +0800

    Update 1.2- addition.py

--- Coursera/1.2- addition.py
+++ Coursera/1.2- addition.py
@@ -1,3 +1,28 @@
+help(math.ceil)
+Help on built-in function ceil in module math:
+
+ceil(...)
+    ceil(x)
+    
+    Return the ceiling of x as an Integral.
+    This is the smallest integer >= x.
+
+
+math.ceil(4.6)
+Out[11]: 5
+
+math.floor(4.6)
+Out[12]: 4
+
+#ceil()向上取整
+
+#floor()向下取整
+
+import random
+
+print(random.choice(['霸刀总裁风','冷艳高贵风','扎心老铁风','人来疯']))
+冷艳高贵风
+
 from datetime import date
 
 date.today()
@@ -33,3 +58,25 @@ Tuesday, October, 10/24/17, 2017 21:52
 
 print(dt.strftime('%a, %b, %d, %Y %H:%M'))
 Tue, Oct, 24, 2017 21:52
+
+dt = datetime(2017,10,24, 21,59)
+
+dt
+Out[30]: datetime.datetime(2017, 10, 24, 21, 59)
+
+print(dt)
+2017-10-24 21:59:00
+
+#把这个时间转成时间戳 timestamp
+
+ts = dt.timestamp()
+
+ts
+Out[34]: 1508853540.0
+
+#把时间戳转成本地时间和日期
+
+print(datetime.fromtimestamp(ts))
+2017-10-24 21:59:00
+
+#用timestamp统一全球所有计算机的时间。从1970年1月1日0时0时区开始计算
