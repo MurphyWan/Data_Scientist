4143d0e6c0c55119bfb214e604078d3298d92dac
Murphy Fei Wan <MurphyWan@users.noreply.github.com>
2017-10-24 15:22:16 +0800

    Add files via upload

---
+++ Coursera/1.1-1.1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 15:00:08 2017
+
+@author: user
+"""
+
+fn = input('Input your first name:')
+print(fn)
+ln = input('Input your last name:')
+print(ln)
+print('Your full name is :', fn + ln)
