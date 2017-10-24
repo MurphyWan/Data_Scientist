0f214324b3a47642bba70140e19c0f4c9e1c76cd
Murphy Fei Wan <MurphyWan@users.noreply.github.com>
2017-10-24 15:29:41 +0800

    Create 1.1-1.1-testing.py

---
+++ Coursera/1.1-1.1-testing.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+print('hello world')
+
+print('''hello
+world''')
+
+print('hello \
+world')
+
+
+
+# ----------output is as below-----------------
+
+hello world
+hello world
+hello
+world
+hello world
