7c821946207a0ab6e10cfac282174772c262f369
Murphy Fei Wan <MurphyWan@users.noreply.github.com>
2017-11-16 16:26:55 +0800

    Update 2.2.3.4_Create_Bars.py

--- DataScientistCourse/Certificate/DataAnalysis-Visualization/2.2.3.4_Create_Bars.py
+++ DataScientistCourse/Certificate/DataAnalysis-Visualization/2.2.3.4_Create_Bars.py
@@ -1,7 +1,9 @@
 '''
 ## 这节讲 pyplot.bar() or Axes.bar()画柱图；Axes.bar()有2个必要参数，和1个可选参数。必填：left和heigt，可选是width
 ## 方法是：
-plt.bar(*left, *height, *width)
+1、用函数plt.bar(*left, *height, *width)
+2、left可以用arnage()+C，来定义
+3、height可以通过DF1中取指定fields和一行(iloc)记录来定义
 '''
 
 import matplotlib.pyplot as plt
